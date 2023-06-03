8eca66af0bb8acaefeb659cb3f4d1ac4203415ed
Shivay2243 <nagarajanlap@gmail.com>
2023-06-03 19:57:33 +0530

    Updated in ChannelDetail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -23,7 +23,7 @@ const ChannelDetail = () => {
         <ChannelCard channelDetail={channelDetail} marginTop="-125px" Width="100%"/>
       </Box>
       <Box display="flex" p="2">
-        <Box sx={{mr: {sm: '100px'}}} />
+        <Box />
           <Videos videos={videos} />
       </Box>
     </Box>
