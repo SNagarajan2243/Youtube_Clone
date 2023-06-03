ac32eb38c6c171ef8d0be072b350daf2a9a8743d
Shivay2243 <nagarajanlap@gmail.com>
2023-06-03 20:11:17 +0530

    Changes in Key

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://youtube-v31.p.rapidapi.com/';
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '1f14b80557msh53754ea925c7496p1e9f6bjsn443eb8e3cf60',
+		'X-RapidAPI-Key': '51fdb35a13msh5a5eae7dd295315p10c141jsn7d8db5030106',
 		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
 	}
 }
